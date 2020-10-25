9ca6b1a16f2e4d806202f323e170f466eaa462f8
tktcorporation <tktcorporation.go@gmail.com>
2020-10-26 06:15:10 +0900

    pass a test with pytest

---
+++ app/tests/__init__.py
@@ -0,0 +1,3 @@
+import sys
+sys.path.append('../src/domain/')
+from src import *
---
+++ app/tests/test_phone.py
@@ -0,0 +1,5 @@
+from src.domain.phone import Phone
+import pytest
+
+def test_initialze():
+    assert Phone("sample").get_name() == "sample", "名前を初期化する"
